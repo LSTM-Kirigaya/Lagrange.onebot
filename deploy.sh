858e88cddd97d207a86fb733aa8db340717313ef
kirigaya <Kirigaya>
2025-10-21 19:26:06 +0800

    update

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+rm -rf /home/kirigaya/project/Lagrange.RagBot/node/bot/node_modules/lagrange.onebot
+npm run build
+cp -rf dist /home/kirigaya/project/Lagrange.RagBot/node/bot/node_modules/lagrange.onebot
